36fb4dbe7cbff79bc280363b5dfb9b1dc939a5f5
mark-kanaris <mark.kanaris@kellogg.northwestern.edu>
2026-03-16 23:16:48 +0000

    created sessions/login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,27 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by({ "email" => params["email"] })
+    if @user != nil
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        session["user_id"] = @user["id"]
+        flash["notice"] = "Welcome."
+        redirect_to "/places"
+      else
+        flash["notice"] = "Invalid Login."
+        redirect_to "/login"
+      end
+    else
+      flash["notice"] = "Invlaid Login."
+      redirect_to "/login"
+    end
   end
 
   def destroy
+    session["user_id"] = nil
+    flash["notice"] = "Safe Travels :)"
+    redirect_to "/login"
   end
 end
+
   
