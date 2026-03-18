a1a823d9070a630173dc1b52a6afe2f4c1a0344f
mark-kanaris <mark.kanaris@kellogg.northwestern.edu>
2026-03-18 00:20:01 +0000

    added bootstrap and filtered posts to just that of the logged in user

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -10,6 +10,8 @@ class EntriesController < ApplicationController
     @entry.uploaded_image.attach(params["uploaded_image"])
     @entry["occurred_on"] = params["occurred_on"]
     @entry["place_id"] = params["place_id"]
+    @entry["user_id"] = session["user_id"]
+
     @entry.save
     redirect_to "/places/#{@entry["place_id"]}"
   end
